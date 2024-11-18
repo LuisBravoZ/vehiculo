04eba9ef83b7dcb84ad790096f5c32482f78be72
1312721242 <junior.zamora@uleam.edu.ec>
2024-11-18 08:23:28 -0500

    junior

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,12 @@ use App\Http\Controllers\VehiculoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Routes::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/vehiculo',[VehiculoController::class,'index']);
-Route::get('/vehiculo/{id}',[VehiculoController::class,'registro_unico']);
-Route::post('/vehiculo',[VehiculoController::class,'store']);
-Route::put('/vehiculo/{id}',[VehiculoController::class,'update']);
-Route::delete('/vehiculo/{id}',[VehiculoController::class,'destroy']);
+Routes::get('/vehiculo',[VehiculoController::class,'index']);
+Routes::get('/vehiculo/{id}',[VehiculoController::class,'registro_unico']);
+Routes::post('/vehiculo',[VehiculoController::class,'store']);
+Routes::put('/vehiculo/{id}',[VehiculoController::class,'update']);
+Routes::delete('/vehiculo/{id}',[VehiculoController::class,'destroy']);
